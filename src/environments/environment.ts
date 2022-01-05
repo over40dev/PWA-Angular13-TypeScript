bfc6323918fb829eb7e9d0962d597ebf7f16143b
Cogentx <code@cogentx.com>
2022-01-05 10:17:20 -0330

    Add Firebase Firestore DB to Angular App

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'angular-pwa-kanban',
+    appId: '1:667976450714:web:545c7c631d7d5939675523',
+    databaseURL: 'https://angular-pwa-kanban.firebaseio.com',
+    storageBucket: 'angular-pwa-kanban.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyBk5mo59Jo46F_iffg5wQoHpquQ5Mk2DAw',
+    authDomain: 'angular-pwa-kanban.firebaseapp.com',
+    messagingSenderId: '667976450714',
+    measurementId: 'G-JNWYWH98JD',
+  },
   production: false
 };
 
