e713821c2816eb48842289fbaba548532314e8d4
Cogentx <code@cogentx.com>
2022-01-14 17:01:08 -0330

    Bug Fix: Invalid Null in Firebase-Auth currentUser

--- src/app/user/auth.guard.ts
+++ src/app/user/auth.guard.ts
@@ -1,29 +1,49 @@
-import { Injectable } from '@angular/core';
-import { Auth } from '@angular/fire/auth';
+import { ThrowStmt } from '@angular/compiler';
+import { Injectable, OnDestroy } from '@angular/core';
+import { Auth, authState, onAuthStateChanged, User } from '@angular/fire/auth';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { SnackService } from '../services/snack.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, OnDestroy {
 
   constructor(
-    private readonly auth:Auth,
-    private readonly snackBar: SnackService){}
+    private readonly auth: Auth,
+    private readonly snackBar: SnackService) { }
+
+  ngOnDestroy(): void {
+    throw new Error('Method not implemented.');
+  }
 
   async canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Promise<boolean> {
 
-    const user = await this.auth.currentUser;
-    const isLoggedIn = !!user;
-
-    if (!isLoggedIn) {
-      this.snackBar.authError('Please Login to Access');
-    }
+      let isLoggedIn = false;
       
-    return isLoggedIn;
-  }
+      // wrap in Arrow Function to avoid 'uninitialized null' result (see note below)
+      const user = () => this.auth.currentUser;
+      
+      isLoggedIn = !!user;
+      
+      !isLoggedIn && this.snackBar.authError('Please Login to Access');
+      
+      return isLoggedIn;
+      
+      /*
+        You can get the currently signed-in user by using the 
+        currentUser property. If a user isn't signed in, currentUser is null.
+        CAUTION: currentUser might also be null because the auth object 
+        has not finished initializing. 
   
+        To Avoid this issue wrap in Arrow Function or use Observable.
+        
+        If you use an observer (not needed in this case) to keep track of 
+        the user's sign-in status, you don't need handle invalid null.
+        */
+  }
+
 }
