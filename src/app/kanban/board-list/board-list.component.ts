49ad70669793119ffdcbabdafb9f1b9758d3f929
Cogentx <code@cogentx.com>
2022-01-17 16:40:58 -0330

    Trigger Board dialog from Board-list Component

--- src/app/kanban/board-list/board-list.component.ts
+++ src/app/kanban/board-list/board-list.component.ts
@@ -1,35 +1,49 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { CdkDragDrop, moveItemInArray } from "@angular/cdk/drag-drop";
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { Subscription } from 'rxjs';
 import { Board } from '../board.model';
 import { BoardService } from '../board.service';
+import { MatDialog } from '@angular/material/dialog';
+import { BoardDialogComponent } from '../dialogs/board-dialog.component';
 
 @Component({
   selector: 'app-board-list',
   templateUrl: './board-list.component.html',
-  styleUrls: ['./board-list.component.scss']
+  styleUrls: ['./board-list.component.scss'],
 })
 export class BoardListComponent implements OnInit, OnDestroy {
   boards: Board[];
   sub: Subscription;
 
-  constructor(public boardService: BoardService) {
+  constructor(public boardService: BoardService, public dialog: MatDialog) {
     this.boards = [];
     this.sub = this.boardService
       .getUserBoards()
-      .subscribe(boards => this.boards = boards);
-   }
-
-  ngOnInit(): void {
+      .subscribe((boards) => (this.boards = boards));
   }
 
+  ngOnInit(): void {}
+
   ngOnDestroy(): void {
-      this.sub.unsubscribe();
+    this.sub.unsubscribe();
   }
 
   drop(event: CdkDragDrop<string[]>) {
-    moveItemInArray(this.boards,event.previousIndex,event.currentIndex);
+    moveItemInArray(this.boards, event.previousIndex, event.currentIndex);
     this.boardService.sortBoards(this.boards);
   }
 
+  openBoardDialog(): void {
+    const dialogRef = this.dialog.open(BoardDialogComponent, {
+      width: '400px',
+      data: {},
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      this.boardService.createBoard({
+        title: result,
+        priority: this.boards.length,
+      });
+    });
+  }
 }
