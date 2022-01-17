438e0a110bb64ab8d1cd379520144eabf2ed52c5
Cogentx <code@cogentx.com>
2022-01-17 20:07:01 -0330

    Create Task Dialog Component for create / update

--- src/app/kanban/dialogs/task-dialog.component.ts
+++ src/app/kanban/dialogs/task-dialog.component.ts
@@ -1,20 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-task-dialog',
   template: `
-    <p>
-      task-dialog works!
-    </p>
+    <h1 mat-dialog-title>Task</h1>
+    <div mat-dialog-content class="content">
+      <mat-form-field>
+        <textarea
+          matInput
+          placeholder="Task description"
+          [(ngModel)]="data.task.description"
+        ></textarea>
+      </mat-form-field>
+      <br />
+      <mat-button-toggle-group
+        #group="matButtonToggleGroup"
+        [(ngModel)]="data.task.label"
+      >
+        <mat-button-toggle *ngFor="let opt of labelOptions" [value]="opt" ]>
+          <mat-icon [ngClass]="opt">{{
+            opt === 'gray' ? 'check_circle' : 'lens'
+          }}</mat-icon>
+        </mat-button-toggle>
+      </mat-button-toggle-group>
+    </div>
+    <div mat-dialog-actions>
+      <button mat-button (click)="onNoClick()">Cancel</button>
+      <button mat-button [mat-dialog-close]="data" cdkFocusInitial>
+        {{ data.isNew ? 'Add Task' : 'Update Task' }}
+      </button>
+    </div>
   `,
-  styles: [
-  ]
+  styles: [],
 })
-export class TaskDialogComponent implements OnInit {
+export class TaskDialogComponent {
+  labelOptions = ['purple', 'blue', 'green', 'yellow', 'red', 'gray'];
 
-  constructor() { }
+  constructor(
+    public dialogRef: MatDialogRef<TaskDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {}
 
-  ngOnInit(): void {
+  onNoClick() {
+    this.dialogRef.close();
   }
-
 }
