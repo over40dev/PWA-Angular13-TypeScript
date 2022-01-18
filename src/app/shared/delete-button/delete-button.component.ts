5258ecefbb294fe8bdc09c688a368cfa8cbe03d0
Cogentx <code@cogentx.com>
2022-01-17 21:54:47 -0330

    Create delete button (dumb) component

--- src/app/shared/delete-button/delete-button.component.ts
+++ src/app/shared/delete-button/delete-button.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
+/**
+ * The delete button component is just UI (dumb component), meaning it only emits an event with the user’s delete intention. The parent component handles the actual database write.
+ */
 @Component({
   selector: 'app-delete-button',
   templateUrl: './delete-button.component.html',
-  styleUrls: ['./delete-button.component.scss']
+  styleUrls: ['./delete-button.component.scss'],
 })
-export class DeleteButtonComponent implements OnInit {
+export class DeleteButtonComponent {
+  canDelete: boolean = false;
 
-  constructor() { }
+  @Output() delete = new EventEmitter<boolean>();
 
-  ngOnInit(): void {
+  constructor() {}
+
+  prepareForDelete() {
+    this.canDelete = true;
   }
 
+  cancel() {
+    this.canDelete = false;
+  }
+
+  deleteBoard() {
+    this.delete.emit(true);
+    this.canDelete = false;
+  }
 }
